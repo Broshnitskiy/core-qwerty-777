c9fa7a558ffae2eb2ce7c8f57a783e47e5d16715
Roman Broshnitskiy <romario777v8@gmail.com>
2024-02-11 00:37:23 +0200

    add form

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
 const openModalBtns = document.querySelectorAll('[data-modal-open]');
-// const closeModalBtn = document.querySelector('[data-modal-close]');
+const closeModalBtn = document.querySelector('[data-modal-close]');
 const backdrop = document.querySelector('[data-backdrop]');
 
 openModalBtns.forEach(function (btn) {
@@ -9,7 +9,7 @@ openModalBtns.forEach(function (btn) {
 function openModal() {
   backdrop.classList.remove('is-hidden');
   document.body.style.overflowY = 'hidden';
-  //   closeModalBtn.addEventListener('click', closeModal);
+  closeModalBtn.addEventListener('click', closeModal);
   document.addEventListener('keydown', handleKeyPress);
   backdrop.addEventListener('click', handleBackgropClick);
 }
@@ -18,7 +18,7 @@ function closeModal() {
   backdrop.classList.add('is-hidden');
   document.body.style.overflowY = 'visible';
   document.removeEventListener('keydown', handleKeyPress);
-  //   closeModalBtn.removeEventListener('click', closeModal);
+  closeModalBtn.removeEventListener('click', closeModal);
   backdrop.removeEventListener('click', handleBackgropClick);
 }
 
